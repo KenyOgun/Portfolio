8f10e25339e9d5abf9941cd8fc072c9fd567b6f9
KenyOgun <holleraturboi@yahoo.ca>
2022-05-04 15:30:49 -0400

    Added social media icons

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,23 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa"
 
 const Homepage = () => {
     return (
         <>
-            <h1>Kehinde</h1>
+            <section className="homepage">
+                <div className="overlay">
+                    <h1>Kehinde</h1>
+                    <p>Front-end Web Developer</p>
+
+                    <ul>
+                        <li><a href="https://www.linkedin.com/in/kehinde-ogunnigbo-75491a45/"
+                            target="_blank" rel="noopenner noreferrer"><FaLinkedin /></a></li>
+                        <li><a href="https://github.com/KenyOgun"
+                            target="_blank" rel="noopenner noreferrer"><FaGithub /></a></li>
+                    </ul>
+                </div>
+            </section>
+
         </>
-
-
     )
 }
 
