9089a51fb063b5c6eb1326281b0bc164cf9531e8
KenyOgun <holleraturboi@yahoo.ca>
2022-05-04 21:35:30 -0400

    added finishing touches

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 // Components
 import Homepage from "./components/Homepage"
+import Menu from "./components/Menu"
 // Pages
 import Projects from "./pages/Projects"
 import Error from "./pages/Error"
+import Contacts from "./pages/Contacts"
 
 const App = () => {
   return (
     <Router>
+      <Menu />
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route exact path="/projects" element={<Projects />} />
+        <Route exact path="/contacts" element={<Contacts />} />
         <Route exact path="/error" element={<Error />} />
+
       </Routes>
     </Router>
   )
