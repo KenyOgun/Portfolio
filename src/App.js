4de3785f943e14edf0a7021474eae9f5e72a5d38
KenyOgun <holleraturboi@yahoo.ca>
2022-05-13 11:59:14 -0400

    Added contact component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-const App = () => {
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
+function App() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Hello</h1>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
     </div>
   );
-};
+}
 
 export default App;
