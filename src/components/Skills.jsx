eba7a02e2dd00f39492581506f7d257e9c2587e7
KenyOgun <holleraturboi@yahoo.ca>
2022-05-27 20:37:31 -0400

    updated icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,7 +18,7 @@ const Skills = () => {
           <p className="text-4xl font-bold inline border-b-4 border-green-600 ">
             Skills
           </p>
-          <p className="py-4">// These are the technologies I've worked with</p>
+          <p className="py-4"> These are the technologies I've worked with</p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
@@ -52,7 +52,7 @@ const Skills = () => {
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Mongo} alt="Mongo icon" />
-            <p className="my-4">MONGO DBB</p>
+            <p className="my-4">MONGO DB</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={AWS} alt="AWS icon" />
