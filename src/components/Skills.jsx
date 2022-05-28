2d2ae042953e8b43649ae1663ee7e50bb0435379
KenyOgun <holleraturboi@yahoo.ca>
2022-05-27 20:44:11 -0400

    cleaned up skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,9 +5,7 @@ import CSS from "../assets/css.png";
 import JavaScript from "../assets/javascript.png";
 import ReactImg from "../assets/react.png";
 import Python from "../assets/python.png";
-import AWS from "../assets/aws.png";
 import NextJS from "../assets/nextjs.png";
-import Mongo from "../assets/mongo.png";
 
 const Skills = () => {
   return (
@@ -21,7 +19,7 @@ const Skills = () => {
           <p className="py-4"> These are the technologies I've worked with</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
             <p className="my-4">HTML</p>
@@ -49,14 +47,6 @@ const Skills = () => {
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Python} alt="Python icon" />
             <p className="my-4">PYTHON</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Mongo} alt="Mongo icon" />
-            <p className="my-4">MONGO DB</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={AWS} alt="AWS icon" />
-            <p className="my-4">AWS</p>
           </div>
         </div>
       </div>
