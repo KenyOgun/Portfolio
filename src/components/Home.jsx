74cd8fe446ee185b36fa803a771dfa516fa50f30
KenyOgun <holleraturboi@yahoo.ca>
2022-06-07 00:15:48 -0400

    minor change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,12 +11,12 @@ const Home = () => {
           Kehinde.
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Full Stack Developer.
+          I'm a Front end Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           I’m a full-stack developer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive full-stack web applications.
+          building responsive front-end web applications.
         </p>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-green-600 hover:border-green-600">
